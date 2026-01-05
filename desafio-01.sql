cd5e6453fc51d73b93263c40e2b33c6184daa71e
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-05 15:08:31 -0300

    Modify clientes table to use VARCHAR types

--- desafio-01.sql
+++ desafio-01.sql
@@ -1,5 +1,6 @@
 CREATE TABLE clientes (
-  id INTEGER PRIMARY KEY,
-  nome TEXT,
-  email TEXT
-  );
+ id INTEGER PRIMARY KEY,
+ nome VARCHAR(100), -- Limite de 100 caracteres (ninguém tem um nome maior que isso, certo?)
+ email VARCHAR(150) -- Espaço extra para aqueles e-mails corporativos gigantescos
+);
+
